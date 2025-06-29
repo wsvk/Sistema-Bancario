11efcb306acd9472ccf2b32873a109b320a31115
mila <camilaferraric1@gmail.com>
2025-06-29 11:37:38 -0300

    fazer health-servlet usar JSP

--- src/main/java/uff/ic/devweb/sistemabancario/HealthServlet.java
+++ src/main/java/uff/ic/devweb/sistemabancario/HealthServlet.java
@@ -4,6 +4,7 @@
  */
 package uff.ic.devweb.sistemabancario;
 
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -31,18 +32,11 @@ public class HealthServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HealthServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>OK</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        request.setAttribute("status", "OK");
+        
+        request.getRequestDispatcher("Health.jsp")
+                .forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ src/main/webapp/Health.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : Health
+    Created on : Jun 28, 2025, 10:36:53 PM
+    Author     : mila
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Ok!!!</h1>
+        <%= request.getAttribute("status") %>
+    </body>
+</html>
